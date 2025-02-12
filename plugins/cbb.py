33991c9ba5b6f03c5a98a2707939787ca116136f
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2025-02-12 23:59:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/TelexMoviesFilesStorebot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/TelexOriginals'>Telex Originals</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Professor</a>",
+            text = f"<b><blockquote>❃ 💓 ᴏᴡɴᴇʀ (ᴀʀʏᴀ) : <a href=https://t.me/Arya_Bro>ᴀʀʏᴀ ʙʀᴏ❤‍🔥</a>\n❃ 🫡ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/Telugu_movies_999>ᴛᴇʟᴜɢᴜᴍᴏᴠɪᴇs𝟿𝟿𝟿 ❣️</a>\n❃ 🥵 ʟ€@ᴋ$: <a href=https://t.me/+4QSB2tPk-ME2NDdl>ᴄʟɢ ɢɪʀʟ ᴀɴᴅ ʟᴜᴠʀs 😛</a>\n❃ 🔞 ᴀᴅᴜʟᴛ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/+aph6xGmeXgU2NzFl>ᴀᴅᴜʟᴛ ᴍᴏᴠɪᴇs 🤤</a>\n❃ 🌿ʀᴇǫᴜᴇsᴛ ɢʀᴏᴜᴘ : <a href=https://t.me/+-duU_vRUZzswZDY1>ɢʀᴏᴜᴘ 🫧</a>\n❃ 🫰 ғɪʟᴛᴇʀ ʙᴏᴛ : <a href=https://t.me/Aryas_Movies_Finder_bot>ғɪʟᴛᴇʀ ʙᴏᴛ 🫶</a></blockquote></b>"
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("🪿 Close", callback_data = "close")
                     ]
                 ]
             )
