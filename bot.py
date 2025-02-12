49693540b1256a7f6999934723081d73440e3a4e
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2025-02-13 01:45:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,6 @@ import pyrogram.utils
 
 pyrogram.utils.MIN_CHANNEL_ID = -1009999999999
 
-
-
 class Bot(Client):
     def __init__(self):
         super().__init__(
@@ -37,11 +35,12 @@ class Bot(Client):
                     link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
                 self.invitelink = link
             except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel !")
-                self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support For Support")
+                self.LOGGER.warning(f"Error: {a}")
+                self.LOGGER.warning(f"Bot Can't Export Invite link From Force Sub Channel!")
+                self.LOGGER.warning(f"Please Double Check The FORCE_SUB_CHANNEL Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER.info("Bot Stopped. Join https://t.me/Telugu_Movies_999 For Support")
                 sys.exit()
+
         if FORCE_SUB_CHANNEL2:
             try:
                 link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
@@ -50,10 +49,10 @@ class Bot(Client):
                     link = (await self.get_chat(FORCE_SUB_CHANNEL2)).invite_link
                 self.invitelink2 = link
             except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel !")
-                self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL2 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support For Support")
+                self.LOGGER.warning(f"Error: {a}")
+                self.LOGGER.warning(f"Bot Can't Export Invite link From Force Sub Channel!")
+                self.LOGGER.warning(f"Please Double Check The FORCE_SUB_CHANNEL2 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL2}")
+                self.LOGGER.info("Bot Stopped. Join https://t.me/Telugu_Movies_999 For Support")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL3:
@@ -64,10 +63,10 @@ class Bot(Client):
                     link = (await self.get_chat(FORCE_SUB_CHANNEL3)).invite_link
                 self.invitelink3 = link
             except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel !")
-                self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL3 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL3}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support For Support")
+                self.LOGGER.warning(f"Error: {a}")
+                self.LOGGER.warning(f"Bot Can't Export Invite link From Force Sub Channel!")
+                self.LOGGER.warning(f"Please Double Check The FORCE_SUB_CHANNEL3 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL3}")
+                self.LOGGER.info("Bot Stopped. Join https://t.me/Telugu_Movies_999 For Support")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL4:
@@ -78,28 +77,29 @@ class Bot(Client):
                     link = (await self.get_chat(FORCE_SUB_CHANNEL4)).invite_link
                 self.invitelink4 = link
             except Exception as a:
-                self.LOGGER(__name__).warning(a)
-                self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel !")
-                self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL4 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL4}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/MadflixBots_Support For Support")
+                self.LOGGER.warning(f"Error: {a}")
+                self.LOGGER.warning(f"Bot Can't Export Invite link From Force Sub Channel!")
+                self.LOGGER.warning(f"Please Double Check The FORCE_SUB_CHANNEL4 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL4}")
+                self.LOGGER.info("Bot Stopped. Join https://t.me/Telugu_Movies_999 For Support")
                 sys.exit()
 
         try:
             db_channel = await self.get_chat(CHANNEL_ID)
             self.db_channel = db_channel
-            test = await self.send_message(chat_id = db_channel.id, text = "Hey 🖐")
+            test = await self.send_message(chat_id=db_channel.id, text="Hey 🖐")
             await test.delete()
         except Exception as e:
-            self.LOGGER(__name__).warning(e)
-            self.LOGGER(__name__).warning(f"Make Sure Bot Is Admin In DB Channel, And Double Check The CHANNEL_ID Value, Current Value: {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/MadflixBots_Support For Support")
+            self.LOGGER.warning(f"Error: {e}")
+            self.LOGGER.warning(f"Make Sure Bot Is Admin In DB Channel, And Double Check The CHANNEL_ID Value, Current Value: {CHANNEL_ID}")
+            self.LOGGER.info("Bot Stopped. Join https://t.me/Telugu_Movies_999 For Support")
             sys.exit()
 
-        self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated By \nhttps://t.me/Madflix_Bots")
-        self.LOGGER(__name__).info(f"""ミ💖 MADFLIX BOTZ 💖彡""")
+        self.parse_mode = ParseMode.HTML
+        self.LOGGER.info("Bot Running..!\n\nCreated By \nhttps://t.me/Telugu_Movies_999")
+        self.LOGGER.info("ミ💖 Telugu Movies 999 💖彡")
         self.username = usr_bot_me.username
-        #web-response
+
+        # web-response
         app = web.AppRunner(await web_server())
         await app.setup()
         bind_address = "0.0.0.0"
@@ -107,12 +107,7 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot Stopped...")
-            
-
-
-
-
+        self.LOGGER.info("Bot Stopped...")
 
 # Jishu Developer 
 # Don't Remove Credit 🥺
