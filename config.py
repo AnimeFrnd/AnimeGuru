b40871870ae0daab84e34b3b6d44afdfc050818e
AnimeFrnd <163717030+AnimeFrnd@users.noreply.github.com>
2025-03-23 03:10:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,20 +51,20 @@ BOT_STATS_TEXT = "<b>BOT UPTIME :</b>\n{uptime}"
 
 USER_REPLY_TEXT = "❌sᴏʀʀʏ మావా నువ్వు నా ᴏᴡɴᴇʀ కాదు..!😜\n\n❌Don't Send Me Messages Directly I'm Only File Share Bot !"
 
-START_MSG = """<b><blockquote>
-Hey, <a href='tg://user?id={id}'>{first}</a>✌🏻. I hope you're feeling the power of 𝐒ʜᴀᴅᴏᴡ Mᴏɴᴀʀᴄʜ 😈.
+START_MSG = """<blockquote>
+<b>Hey, <a href='tg://user?id={id}'>{first}</a>✌🏻. I hope you're feeling the power of 𝐒ʜᴀᴅᴏᴡ Mᴏɴᴀʀᴄʜ 😈.</b>
 
-I'm The Ultimate File Sharing Bot, built to rule the Shadow Realm 🖤
+<b>I'm The Ultimate File Sharing Bot, built to rule the Shadow Realm 🖤</b>
 
-🔱 Store & Share Files with a Single Click.  
-🛡️ Infinite File Management System.  
-📂 Post Files in Anime Monarch 👑 Template.
+🔱 <b>Store & Share Files with a Single Click.</b>  
+🛡️ <b>Infinite File Management System.</b>  
+📂 <b>Post Files in Anime Monarch 👑 Template.</b>
 
 ---
 
-Now, The File Realm Is Under My Control 😈.  
-Are You Ready to Dominate, {first}-Sama? 👑
-</blockquote></b>"""
+<b>Now, The File Realm Is Under My Control 😈.</b>  
+<b>Are You Ready to Dominate, {first}-Sama? 👑</b>
+</blockquote>"""
 
 FORCE_MSG = "Join my channels first 😈"
 
